e63f702d86bbbb8517db6cff3247f096e378fc54
Terence <weixiang.tan@redalphacyber.com>
2025-08-06 13:49:20 +0800

    Upload test.py and update main.py

--- main.py
+++ main.py
@@ -2,6 +2,3 @@ print("bbq stingray the goat")
 
 def add(a, b):
 	return a + b
-
-def test():
-	assert add(3, 5) == 8
---
+++ test.py
@@ -0,0 +1,7 @@
+from main import add
+
+def test():
+        assert add(3, 5) == 8
+
+if (__name__) == (__main__):
+	test()
